315b7c2275b0940249cd9b1974cb246f099f10d5
josue recoba <josue.huer.reco.22@gamil.com>
2022-02-25 23:25:53 -0600

    Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
+import {AddCategory} from "./components/AddCategory";
 
 export const GifExpertApp = () => {
-    //const categorias = ['One punch', 'Samurai x', 'Dragon Ball'];
+
     const [categorias, setCategorias]= useState(['One punch', 'Samurai x', 'Dragon Ball']);
     
-    const handleAdd = () =>{
-        setCategorias(['HunterXHunter', ...categorias,] );
+    // const handleAdd = () =>{
+    //   setCategorias(['HunterXHunter', ...categorias,] );
 
-    }
-
+    
+   
     return(
         <>
             <h2>GifExperApp</h2>
+            <AddCategory/>
             <hr />
 
-            <button onClick={handleAdd}>Agregar</button>
+            
             <ol>
                 {
                     categorias.map( category => {
